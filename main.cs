1866d3d855c658424db29dd8fd5f37da4b453bb1
Teller026 <max.de.hom@gmail.com>
2026-05-14 00:43:35 +0300

    Refactor BoolArray class and add Program class

--- main.cs
+++ main.cs
@@ -1,28 +1,60 @@
 using System;
 
-public class BoolArray
+namespace TaskApp
 {
-    private bool[] _data;
-    public BoolArray(int size)
+    public class BoolArray
     {
-        _data = new bool[size];
-    }
-    public bool this[int index]
-    {
-        get
+        private bool[] _data;
+        public BoolArray(int size)
         {
-            if (index < 0 || index >= _data.Length)
-            {
-                throw new IndexOutOfRangeException();
-            }
-            return !_data[index];
+            _data = new bool[size];
         }
-        set
+        public bool this[int index]
         {
-            if (index >= 0 && index < _data.Length)
+            get
             {
-                _data[index] = value;
+                if (index < 0 || index >= _data.Length)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                return !_data[index];
             }
+            set
+            {
+                if (index >= 0 && index < _data.Length)
+                {
+                    _data[index] = value;
+                }
+            }
+        }
+        public bool State
+        {
+            get
+            {
+                bool result = false;
+                foreach (bool item in _data)
+                {
+                    result |= item;
+                    if (result) break;
+                }
+                return result;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            BoolArray container = new BoolArray(3);
+            
+            container[0] = true;
+            container[1] = false;
+            container[2] = false;
+
+            Console.WriteLine(container[0]);
+            Console.WriteLine(container[1]);
+            Console.WriteLine(container.State);
         }
     }
 }
