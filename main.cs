d1fd19a808f8b9b00e78a7d37fab2eb4049e9dea
Teller026 <max.de.hom@gmail.com>
2026-05-14 00:41:04 +0300

    Add indexer to BoolArray class

--- main.cs
+++ main.cs
@@ -7,4 +7,22 @@ public class BoolArray
     {
         _data = new bool[size];
     }
+    public bool this[int index]
+    {
+        get
+        {
+            if (index < 0 || index >= _data.Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            return !_data[index];
+        }
+        set
+        {
+            if (index >= 0 && index < _data.Length)
+            {
+                _data[index] = value;
+            }
+        }
+    }
 }
